de02d3fd7074e621f0babe7c080cb919b9c0b8ba
Mars Hall <mars@heroku.com>
2021-09-28 08:06:47 -0700

    Add /inspeq-chunked-response endpoint to test chunked responses in a client

--- server.js
+++ server.js
@@ -8,7 +8,40 @@ const app = express();
 
 app.use(bodyParser.raw({ type: "*/*" }))
 
+app.get('/inspeq-chunked-response', (req, res) => {
+  logRequest(req);
+
+  res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
+  res.setHeader('Transfer-Encoding', 'chunked');
+  res.setHeader('Cache-Control', 'no-cache');
+
+  res.write(`Streaming response…\n`);
+
+  let chunkCount = 0;
+  intervalSig = setInterval(function() {
+    chunkCount += 1;
+    res.write(`Chunk ${chunkCount}\n`);
+    if (chunkCount === 10) {
+      clearInterval(intervalSig);
+      res.write(`Response complete.\n`);
+      res.end();
+    }
+  }, 1000);
+});
+
 app.all('*', (req, res) => {
+  logRequest(req);
+
+  // Always respond successful.
+  res.sendStatus(responseStatus);
+});
+
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`Listening on http://localhost:${port}`);
+});
+
+function logRequest(req) {
   // Collect querystring & headers for output.
   const headers = []
   for (let k in req.headers) {
@@ -24,12 +57,4 @@ app.all('*', (req, res) => {
 ➡️  ${req.method} ${req.path}${query.length > 0 ? `?${query.join("&")}` : ""}
 ${headers.join("\n")}${req.body.constructor === Object && Object.keys(req.body).length === 0 ? "" : `\n\n${req.body}`}
 `);
-
-  // Always respond successful.
-  res.sendStatus(responseStatus);
-});
-
-app.listen(port, (err) => {
-  if (err) throw err;
-  console.log(`Listening on http://localhost:${port}`);
-});
+}
