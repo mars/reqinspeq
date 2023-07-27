f85966c6117e6fbdacf57c29325ccf3b41f36e40
Mars Hall <mars@heroku.com>
2023-07-27 16:07:08 -0700

    Enable CORS

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser')
 
 const port = process.env.PORT || 3333;
@@ -6,6 +7,7 @@ const responseStatus = +(process.env.RESPONSE_STATUS || 200);
 
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.raw({ type: "*/*" }))
 
 app.get('/inspeq-chunked-response', (req, res) => {
